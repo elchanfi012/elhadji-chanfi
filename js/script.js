54b9b60136de55f5983942f10f2915cc54f08650
elhadji <el-hadji012@live.fr>
2021-02-08 19:52:06 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.experiences').slick({
+        dots: false,
+        arrows: true,
+        autoplay: true,
+        autplaySpeed: 100,
+        slidesToShow: 1
+    });
+  });
